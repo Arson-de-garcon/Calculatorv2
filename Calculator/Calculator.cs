8f08983757cb50000474ad65ddeeba70c6d068c8
Arseniy <arseniy.malyutin@gmail.com>
2020-09-09 09:13:23 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+
+        public class Calculator
+        {
+            public double Add(double a, double b)
+            {
+                return a + b;
+            }
+
+            public double Subtract(double a, double b)
+            {
+                return a - b;
+            }
+
+            public double Multiply(double a, double b)
+            {
+                return a * b;
+            }
+
+            public double Power(double a, double exp)
+            {
+                return Math.Pow(a, exp);
+            }
+        }
+    
+
+}
